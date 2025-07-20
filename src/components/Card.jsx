37dd84d91a0db146d5092bcdd089d18fb80aa28c
namrata <namrataap725@gmail.com>
2025-07-20 16:38:25 +0545

    Skip rendering cards with missing poster image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,36 +7,42 @@ function Card({ data }) {
 
   return (
     <div className="card-grid">
-      {data.map((movie, index) => (
-        <div className="card" key={index}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="cardContainer">
-            <p>
-              <strong>Original Title:</strong> {movie.original_title}
-            </p>
-            <p>
-              <strong>Movie Name:</strong> {movie.title}
-            </p>
-            <p>
-              <strong>Release Date:</strong> {movie.release_date}
-            </p>
-            <p>
-              <strong>18+ / adult:</strong>{" "}
-              {movie.adult === false ? "No" : "Yes"}
-            </p>
-            <p>
-              <strong>Original Language:</strong> {movie.original_language}
-            </p>
-            <p>
-              <strong>Over View: </strong>
-              {movie.overview}
-            </p>
-          </div>
-        </div>
-      ))}
+      {data.map((movie, index) => {
+        if (!movie || !movie.poster_path) {
+          return null;
+        } else {
+          return (
+            <div className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className="cardContainer">
+                <p>
+                  <strong>Original Title:</strong> {movie.original_title}
+                </p>
+                <p>
+                  <strong>Movie Name:</strong> {movie.title}
+                </p>
+                <p>
+                  <strong>Release Date:</strong> {movie.release_date}
+                </p>
+                <p>
+                  <strong>18+ / adult:</strong>{" "}
+                  {movie.adult === false ? "No" : "Yes"}
+                </p>
+                <p>
+                  <strong>Original Language:</strong> {movie.original_language}
+                </p>
+                <p>
+                  <strong>Over View: </strong>
+                  {movie.overview}
+                </p>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
